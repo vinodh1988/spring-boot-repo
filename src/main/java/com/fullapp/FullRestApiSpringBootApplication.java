a70a854cf0ded6f1e552801c76019cc54a401a48
vinodhtrainer@gmail.com <vinodhtrainer@gmail.com>
2025-12-16 16:38:56 +0530

    latest

---
+++ src/main/java/com/fullapp/FullRestApiSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.fullapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullRestApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullRestApiSpringBootApplication.class, args);
+		System.out.println("Started");
+	}
+
+}
---
+++ src/test/java/com/fullapp/FullRestApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullRestApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
