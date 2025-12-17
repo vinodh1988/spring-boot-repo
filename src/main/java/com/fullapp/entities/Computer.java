b4dbc54f40a07158cf04882abe7d8c9ee89b064e
vinodhtrainer@gmail.com <vinodhtrainer@gmail.com>
2025-12-17 17:12:37 +0530

    latest

---
+++ src/main/java/com/fullapp/entities/Computer.java
@@ -0,0 +1,77 @@
+package com.fullapp.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="computers")
+public class Computer {
+@Id
+   private Integer cno;
+@Column 
+   private String brand;
+@Column 
+   private String ram;
+@Column
+   private String cpu;
+@Column
+   private String storage;
+
+   public Computer() {
+	super();
+	// TODO Auto-generated constructor stub
+   }
+
+   public Computer(Integer cno, String brand, String ram, String cpu, String storage) {
+	super();
+	this.cno = cno;
+	this.brand = brand;
+	this.ram = ram;
+	this.cpu = cpu;
+	this.storage = storage;
+   }
+
+   public Integer getCno() {
+	return cno;
+   }
+
+   public void setCno(Integer cno) {
+	this.cno = cno;
+   }
+
+   public String getBrand() {
+	return brand;
+   }
+
+   public void setBrand(String brand) {
+	this.brand = brand;
+   }
+
+   public String getRam() {
+	return ram;
+   }
+
+   public void setRam(String ram) {
+	this.ram = ram;
+   }
+
+   public String getCpu() {
+	return cpu;
+   }
+
+   public void setCpu(String cpu) {
+	this.cpu = cpu;
+   }
+
+   public String getStorage() {
+	return storage;
+   }
+
+   public void setStorage(String storage) {
+	this.storage = storage;
+   }
+   
+   
+}
